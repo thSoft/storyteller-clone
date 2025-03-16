03a0a428b63e20d1ab87c08a313b8d9376341062
Dénes Harmath <harmathdenes@gmail.com>
2025-03-17 00:52:27 +0100

    Move App

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "./App";
+import App from "./components/App";
 import { persistor, store } from "./store";
 
 const rootElement = document.getElementById("root");
